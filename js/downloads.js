512cef3e2815a213ad52e758576c40a9bf32c9d8
FanzTzyAI <fanztzyai@gmail.com>
2026-06-22 16:23:36 +0700

    Add downloadText function for text file downloads

---
+++ js/downloads.js
@@ -0,0 +1,14 @@
+function downloadText(filename, content){
+
+if(!content) content = "";
+
+const blob = new Blob([content], {type:"text/plain"});
+
+const url = URL.createObjectURL(blob);
+
+const a = document.createElement("a");
+a.href = url;
+a.download = filename;
+a.click();
+
+}
