ea05895500465af01a51b65944bb861c5e417d21
FanzTzyAI <fanztzyai@gmail.com>
2026-06-22 16:12:35 +0700

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,24 +1,24 @@
 function saveSettings(){
 
 const settings = {
-
-displayName:
-document.getElementById(
-"displayName"
-).value,
-
-language:
-document.getElementById(
-"language"
-).value
-
+displayName: document.getElementById("displayName").value,
+language: document.getElementById("language").value
 };
 
-localStorage.setItem(
-"settings",
-JSON.stringify(settings)
-);
+localStorage.setItem("settings", JSON.stringify(settings));
 
 alert("Pengaturan disimpan");
+
+}
+
+function resetAll(){
+
+if(confirm("Reset semua data?")){
+
+localStorage.clear();
+alert("Data direset");
+location.reload();
+
+}
 
 }
