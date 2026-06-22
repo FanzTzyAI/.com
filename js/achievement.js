0b81d21caa2a6df735047acd84563027088dc463
FanzTzyAI <fanztzyai@gmail.com>
2026-06-22 16:05:40 +0700

    Create achievement.js

---
+++ js/achievement.js
@@ -0,0 +1,28 @@
+function unlockAchievement(name){
+
+let achievements =
+JSON.parse(
+localStorage.getItem(
+"achievements"
+)
+) || [];
+
+if(
+!achievements.includes(name)
+){
+
+achievements.push(name);
+
+localStorage.setItem(
+"achievements",
+JSON.stringify(
+achievements
+));
+
+addNotification(
+"Achievement baru: " + name
+);
+
+}
+
+}
