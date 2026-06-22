f88ba77b48499655ae9fd470117c61e796bfd04c
FanzTzyAI <fanztzyai@gmail.com>
2026-06-22 16:08:27 +0700

    Add workspace component with textarea and button

---
+++ pages/workspace.js
@@ -0,0 +1,14 @@
+<div class="workspace">
+
+<textarea
+id="workspacePrompt"
+placeholder="Tulis perintah..."
+></textarea>
+
+<button onclick="runWorkspace()">
+Jalankan
+</button>
+
+<div id="workspaceOutput"></div>
+
+</div>
